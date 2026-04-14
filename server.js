10f74cf0e58ef85cbc33dec93f659d91ee096c19
Vickyxsg173 <vikrantvaidya17@gmail.com>
2026-04-14 22:28:21 +0530

    v3 final

--- server.js
+++ server.js
@@ -1043,7 +1043,7 @@ app.delete("/api/admin/messages/:id", ensureAdmin, async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "dist")));
 
-  app.get("*", (req, res) => {
+  app.get("(.*)", (req, res) => {
     res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 }
